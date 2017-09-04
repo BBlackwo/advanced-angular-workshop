5de9219695d1d448d566fc5924aedc173fce7df9
Benjamin Blackwood <benjamin.blackwood@ratemyagent.com>
2017-09-04 19:46:54 +1000

    3. Simple HTTP Caching - Finished

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,7 +1,13 @@
 const BaseEndpoints = {
   THINGS_TO_DO: 'https://melbourne-things-to-do.firebaseio.com',
-}
+};
 
 export const ApiEndpoints = {
   ACTIVITIES: `${BaseEndpoints.THINGS_TO_DO}/activities.json`
+};
+
+const Prefix = 'AAW';
+
+export const StorageKeys = {
+  ACTIVITY_CACHE: `${Prefix}-ActivityCache`
 };
